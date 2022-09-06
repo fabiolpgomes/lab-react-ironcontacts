9c0b1f911ae402f46823be84a33005826c927ba3
Fabio Gomes <fabiolpgomes@hotmail.com>
2022-09-07 01:42:38 +0200

    Interation 1 Display 5 Contacts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"; // src/App.js
+import contacts from "./contacts.json";
+import { useState } from "react"; //importing useState
+
+//function to show only 5 contacts //Importing contacts, total 52 bring first 5 names
+const actorsFive = contacts.splice(0, 5);
 
 function App() {
+  //const and variable, and hooks
+  const [actors, setActors] = useState(actorsFive);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <thead>
+        <tr>
+          <th> Picture </th>
+          <th> Name </th>
+          <th> Popularity </th>
+        </tr>
+      </thead>
+      <tbody>
+        {actors.map((actor) => {
+          return (
+            <tr key={actor.id}>
+              <td>
+                <img classeName="imgActor" src={actor.pictureUrl} alt="Actor" />
+              </td>
+
+              <td> {actor.name} </td>
+              <td> {Number(actor.popularity).toFixed(2)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
     </div>
   );
 }
